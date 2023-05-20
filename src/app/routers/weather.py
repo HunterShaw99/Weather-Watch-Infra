b72654ab8728d809ff99f25d9e05471cd7a2e6cb
Hunter Shaw <huntershaw0@gmail.com>
2023-05-20 12:44:08 -0400

    Major refactor to back-end structure, suited for larger application

--- src/app/routers/weather.py
+++ src/app/routers/weather.py
@@ -1,26 +1,15 @@
-import os
-import sys
-from typing import Union
-from fastapi import FastAPI
-from pydantic import Json
+from fastapi import APIRouter
 
-from .bridge.Bridge import request_aqi, request_forecast
+from ..bridge.Bridge import request_aqi, request_forecast
 
-app = FastAPI()
+weather_router = APIRouter()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}   
-
-
-@app.get("/get/current_forecast")
+@weather_router.get('/current_forecast/', tags=['Weather'])
 async def get_current_forecast(lat: str | None = None, lon: str | None = None, zip_code: int | None = None) -> dict: 
     res = await request_forecast(lat, lon, zip_code)
     return res
 
-
-@app.get("/get/air_quality")
+@weather_router.get('/air_quality/', tags=['Weather'])
 async def get_current_aqi(lat:str|None=None, lon:str|None=None, zip_code:int|None=None) -> dict:
     res = await request_aqi(lat, lon, zip_code)
     return res

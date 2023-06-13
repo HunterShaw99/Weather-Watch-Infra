ce653cd0eee84d3425b26015d1b8fb4d4e3156c9
Hunter Shaw <huntershaw0@gmail.com>
2023-06-12 20:35:00 -0400

    added support for zip and lat lon on api

--- src/app/bridge/Bridge.py
+++ src/app/bridge/Bridge.py
@@ -14,15 +14,18 @@ api_key = os.getenv('API_KEY')
 async def request_forecast(lat=None, lon=None, zip_code=None):
     async with httpx.AsyncClient() as client:
         if zip_code:
-            url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{COUNTRY_CODE}&appid={api_key}'
+            url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}&units=imperial'
         else:
-            url = f''
+            url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial'
         task = await client.get(url)
         return task.json()
 
 
 async def request_aqi(lat=None, lon=None, zip_code=None):
     async with httpx.AsyncClient() as client:
-        url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid={api_key}'
+        if zip_code:
+            url = f'http://api.openweathermap.org/data/2.5/air_pollution?zip={zip_code}&appid={api_key}&units=imperial'
+        else:
+            url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}&units=imperial'
         task = await client.get(url)
         return task.json() 

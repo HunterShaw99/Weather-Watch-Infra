1c5be35555ad1ec178a559f5b61b704b8503bd91
Hunter Shaw <huntershaw0@gmail.com>
2023-07-15 20:29:02 -0400

    Fixed incorrect env key

--- src/app/routers/user_creation.py
+++ src/app/routers/user_creation.py
@@ -9,7 +9,7 @@ usr_router = APIRouter()
 
 @usr_router.post('/account_creation/', tags=['User'], response_model=usr_out)
 async def post_user(usr_details: usr_in) -> Any: 
-    conn = psycopg2.connect(dbname=os.getenv('wwdb'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PASSWORD'), host=os.getenv('POSTGRES_HOST_LOCAL'), port=os.getenv('POSTGRES_PORT'))
+    conn = psycopg2.connect(dbname=os.getenv('POSTGRES_DB'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PASSWORD'), host=os.getenv('POSTGRES_HOST_LOCAL'), port=os.getenv('POSTGRES_PORT'))
     cur = conn.cursor()
     cur.execute('INSERT INTO app.users("user_email", "f_name", "l_name","phone_num","street","zip","city","state") VALUES (%s,%s,%s,%s,%s,%s,%s,%s)',
                 (usr_details.user_email, usr_details.f_name, usr_details.l_name, usr_details.phone_num, usr_details.street, usr_details.u_zip, usr_details.city, usr_details.state))

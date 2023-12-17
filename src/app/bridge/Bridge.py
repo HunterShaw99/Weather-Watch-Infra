1d58dc4666a0091b18da1cdb9350ce04a4b2af78
Hunter Shaw <huntershaw0@gmail.com>
2023-12-16 19:38:52 -0500

    Initial commit

--- src/app/bridge/Bridge.py
+++ src/app/bridge/Bridge.py
@@ -4,23 +4,23 @@ from typing import Union
 from dotenv import load_dotenv
 import httpx
 
-
 load_dotenv()
 
 COUNTRY_CODE = 'us'
 api_key = os.getenv('API_KEY')
-        
+headers = {'User-Agent': 'weather-watch.app, huntershaw0@gmail.com'}
 
-async def request_forecast(lat=None, lon=None, zip_code=None):
-    async with httpx.AsyncClient() as client:
-        if zip_code:
-            url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}&units=imperial'
-        else:
-            url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial'
-        task = await client.get(url)
-        return task.json()
+@staticmethod
+async def request_forecast(lat, lon):
+    async with httpx.AsyncClient(headers=headers) as client:
+        url_gridpoints = f'https://api.weather.gov/points/{lat},{lon}'
+        proxy = await client.get(url_gridpoints)
+        url_forecast = proxy.json()
+        url_forecast = url_forecast['properties']['forecastHourly']
+        proxy = await client.get(url_forecast)
+        return [proxy.json(), proxy.status_code]
 
-
+@staticmethod
 async def request_aqi(lat=None, lon=None, zip_code=None):
     async with httpx.AsyncClient() as client:
         if zip_code:
--- src/app/routers/weather.py
+++ src/app/routers/weather.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Header
 
 from ..bridge.Bridge import request_aqi, request_forecast
 
 weather_router = APIRouter()
 
 @weather_router.get('/current_forecast/', tags=['Weather'])
-async def get_current_forecast(lat: str | None = None, lon: str | None = None, zip_code: int | None = None) -> dict: 
-    res = await request_forecast(lat, lon, zip_code)
-    return res
+async def get_current_forecast(lat:str|None=None, lon:str|None=None) -> dict: 
+    res = await request_forecast(lat[:5], lon[:5])
+    if res[1] != 200:
+        raise HTTPException(status_code=404, detail="too many requests")
+    return res[0]['properties']
 
 @weather_router.get('/air_quality/', tags=['Weather'])
 async def get_current_aqi(lat:str|None=None, lon:str|None=None, zip_code:int|None=None) -> dict:
     res = await request_aqi(lat, lon, zip_code)
+    if res['status'] != 200:
+        raise HTTPException(status_code=404, detail="too many requests")
     return res
